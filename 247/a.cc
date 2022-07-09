db873e35af9844354ee04508d61b6b8c525f45b9
Yoshitsugu Tahara <arisahyper0000@gmail.com>
2022-07-10 08:56:52 +0900

    .

---
+++ 247/a.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve() {
+    string s;
+    cin >> s;
+
+    cout << "0" + s.substr(0,3) << endl;
+}
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+
+    solve();
+    return 0;
+}
